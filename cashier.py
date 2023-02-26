59cba8228e19a29321f18764bf875da40f71c2f2
Achmad Fauzi <jiboncoco@jiboncoco.local>
2023-02-26 10:05:55 +0700

    update

--- cashier.py
+++ cashier.py
@@ -7,7 +7,7 @@ print("Pembeli membayar :")
 total_pay = input()
 
 # set array & set config
-array_fractions = [100000, 50000, 20000, 10000, 5000, 2000, 1000, 500, 200, 100, 50, 25, 10, 5]
+array_fractions = [100000, 75000, 50000, 20000, 10000, 5000, 2000, 1000, 500, 200, 100, 50, 25, 10, 5]
 last_total = 0
 
 def check_cashback():
